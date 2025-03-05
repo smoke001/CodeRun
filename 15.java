6a35a622b92cbbddcdc0f24597593e6fabb63b31
Vlad <57848419+smoke001@users.noreply.github.com>
2025-03-05 22:16:59 +1000

    Create 15.java

---
+++ 15.java
@@ -0,0 +1,88 @@
+package me.smokesh;
+
+import java.io.*;
+
+public class Main {
+
+    /*
+	Для чтения входных данных необходимо получить их
+	из стандартного потока ввода (System.in).
+	Данные во входном потоке соответствуют описанному
+	в условии формату. Обычно входные данные состоят
+	из нескольких строк. Можно использовать более производительные
+	и удобные классы BufferedReader, BufferedWriter, Scanner, PrintWriter.
+
+	С помощью BufferedReader можно прочитать из стандартного потока:
+	* строку -- reader.readLine()
+	* число -- int n = Integer.parseInt(reader.readLine());
+	* массив чисел известной длины (во входном потоке каждое число на новой строке) --
+	int[] nums = new int[len];
+    for (int i = 0; i < len; i++) {
+        nums[i] = Integer.parseInt(reader.readLine());
+    }
+	* последовательность слов в строке --
+	String[] parts = reader.readLine().split(" ");
+
+	Чтобы вывести результат в стандартный поток вывода (System.out),
+	Через BufferedWriter можно использовать методы
+	writer.write("Строка"), writer.write('A') и writer.newLine().
+
+	Возможное решение задачи "Вычислите сумму чисел в строке":
+	int sum = 0;
+    String[] parts = reader.readLine().split(" ");
+    for (int i = 0; i < parts.length; i++) {
+        int num = Integer.parseInt(parts[i]);
+        sum += num;
+    }
+    writer.write(String.valueOf(sum));
+	*/
+
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Node root = null;
+        String[] input = reader.readLine().split(" ");
+
+        for (String numStr : input) {
+            int num = Integer.parseInt(numStr);
+            if (num == 0) {
+                break;
+            }
+            root = insert(root, num);
+        }
+
+        System.out.println(height(root));
+
+        reader.close();
+        writer.close();
+    }
+
+    static class Node {
+        int value;
+        Node left, right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = this.right = null;
+        }
+    }
+
+    static Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+        if (value < root.value) {
+            root.left = insert(root.left, value);
+        } else if (value > root.value) {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+    static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
