e8369bd7946bd29ed838dc6022cd3156a22a19fb
Vlad <57848419+smoke001@users.noreply.github.com>
2025-03-05 18:52:32 +1000

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,60 @@
+package me.smokesh;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    /*
+	Для чтения входных данных необходимо получить их
+	из стандартного потока ввода (System.in).
+	Данные во входном потоке соответствуют описанному
+	в условии формату. Обычно входные данные состоят
+	из нескольких строк. Можно использовать более производительные
+	и удобные классы BufferedReader, BufferedWriter, Scanner, PrintWriter.
+
+	С помощью BufferedReader можно прочитать из стандартного потока:
+	* строку -- reader.readLine()
+	* число -- int n = Integer.parseInt(reader.readLine());
+	* массив чисел известной длины (во входном потоке каждое число на новой строке) --
+	int[] nums = new int[len];
+    for (int i = 0; i < len; i++) {
+        nums[i] = Integer.parseInt(reader.readLine());
+    }
+	* последовательность слов в строке --
+	String[] parts = reader.readLine().split(" ");
+
+	Чтобы вывести результат в стандартный поток вывода (System.out),
+	Через BufferedWriter можно использовать методы
+	writer.write("Строка"), writer.write('A') и writer.newLine().
+
+	Возможное решение задачи "Вычислите сумму чисел в строке":
+	int sum = 0;
+    String[] parts = reader.readLine().split(" ");
+    for (int i = 0; i < parts.length; i++) {
+        int num = Integer.parseInt(parts[i]);
+        sum += num;
+    }
+    writer.write(String.valueOf(sum));
+	*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] parts = reader.readLine().split(" ");
+
+        int[] a = new int[3];
+        for (int i = 0; i < 3; i++) {
+            a[i] = Integer.parseInt(parts[i]);
+        }
+
+        Arrays.sort(a);
+
+        writer.write(String.valueOf(a[1]));
+        writer.newLine();
+        writer.flush();
+
+        reader.close();
+        writer.close();
+    }
+}
